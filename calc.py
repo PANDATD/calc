efb966bd3a72db0760bcc48f30dd2614aab85fa3
MR. TEJAS DIXIT <tejasdixit17@gmail.com>
2021-06-13 12:57:58 +0530

    added substraction feature

--- calc.py
+++ calc.py
@@ -3,3 +3,8 @@ def add(num1:int, num2:int)->int:
 	print(f"Addition of {num1} and {num2} =: {num1+num2}")
 
 add(10,20)
+
+def sub(num:int, num2:int)->int:
+	"""This funcation return substrction of two numbers"""
+	return f"Substraction of two numbers{num1-num2}"
+sub(20,10)
