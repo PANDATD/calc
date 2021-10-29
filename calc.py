e09b7eef256af922627d20394fb28ea920b8af37
MR. TEJAS DIXIT <tejasdixit17@gmail.com>
2021-10-29 15:16:36 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -3,5 +3,9 @@ def add(num1, num2):
   
   
   """ This funcation will return addition of two numbers"""
-  retun f'{num1+         num2}'
-add(1,3)
+  return f'{num1+         num2}     '
+
+
+
+add(1,     
+    3)
