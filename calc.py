25e7d129e2897ba1a8db3fff302c3d9f7a8926da
Rushikesh <59604915+Rushikesh4520@users.noreply.github.com>
2022-10-19 16:45:51 +0530

    Update calc.py
    updated with sub()

--- calc.py
+++ calc.py
@@ -11,3 +11,13 @@ def add(number1: int| float, number2: int| float) -> int | float :
                
 print(help(add))
 add(12,23)
+
+def sub(number1: int, number2 :int )->int :
+    """This Funcation will returns Substraction of Two no
+  
+  Param1: number1: int | float 
+  Param2: number2: int | float 
+  
+  """
+   return print(f"Substraction of Two No's -> {number1-number2}",sep='<------->',end=' <------ END OF PROGRAM',flush=True,)
+sub(10-5)
